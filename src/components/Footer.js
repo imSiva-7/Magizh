d9beb7a80875487ab23b0f177ae8e263efdb8779
Siva RamaKrishnan <sivabxng@gmail.com>
2026-01-09 11:43:44 +0530

    improved css in procuremnet history version: ...4

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -51,7 +51,7 @@ export default function Footer() {
             <span className={styles.copyrightText}>
               © {currentYear} Magizh Dairy. All rights reserved.
             </span>
-            <span className={styles.version}>v1.1.2</span>
+            <span className={styles.version}>v1.1.4</span>
           </div>
 
           {/* <div className={styles.technicalInfo}>
