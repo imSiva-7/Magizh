0f1ddca5e048fc896ea5f3221b5eb817eb69a5cb
Siva RamaKrishnan <sivabxng@gmail.com>
2026-05-15 02:28:27 +0530

    ADDED EXPORT FOR ORDERS - version: .6.4

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -129,7 +129,7 @@ export default function Footer() {
               © {currentYear} Magizh Dairy. All rights reserved.
             </span>
 
-            <span className={styles.version}>v1.6.2</span>
+            <span className={styles.version}>v1.6.4</span>
           </div>
         </div>
       </div>
